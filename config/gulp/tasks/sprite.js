ad983a92e2e8d68aec4892fe525d965c5fe6fcda
SylvainOPO <sylvain.d@opo.fr>
2016-10-25 17:25:19 +0200

    feat (gulp) : Add a directory condition in "sprite" task

--- config/gulp/tasks/sprite.js
+++ config/gulp/tasks/sprite.js
@@ -55,53 +55,59 @@ gulp.task('sprite', ['clean-app-sprites'], () => {
     files.forEach(file => {
       //console.log(file);
 
-      var spriteData = gulp.src(config.assets + 'sprites/'+file+'/**/*.png')
-      //.pipe(debug({title: 'unicorn:'}))
-      .pipe(spritesmith({
-        imgName: file+'.png',
-        cssName: file+'.css',
-        //imgPath: '../sprites/'+file+'.png',
-        cssVarMap: function (item) {
-          //console.log('item', item);
+      if (fs.statSync(config.assets + 'sprites' + '/' + file).isDirectory()) {
 
-          if (item.name.match(/On$/i)) {
-            item.name = item.name.replace(/On$/i, ":hover") + ', .icon-' + item.name.replace(/On$/i, ".hover");
+        console.log('Sprite generation for "' + file + '" directory!');
+
+        var spriteData = gulp.src(config.assets + 'sprites/'+file+'/**/*.png')
+        //.pipe(debug({title: 'unicorn:'}))
+        .pipe(spritesmith({
+          imgName: file+'.png',
+          cssName: file+'.css',
+          //imgPath: '../sprites/'+file+'.png',
+          cssVarMap: function (item) {
+            //console.log('item', item);
+
+            if (item.name.match(/On$/i)) {
+              item.name = item.name.replace(/On$/i, ":hover") + ', .icon-' + item.name.replace(/On$/i, ".hover");
+            }
+
+            if (item.name.match(/-hover$/i)) {
+              item.name = item.name.replace(/-hover$/i, ":hover") + ', .icon-' + item.name.replace(/-hover$/i, ".hover");
+            }
+
+            if (item.name.match(/roll$/i)) {
+              item.name = item.name.replace(/roll$/i, ":hover") + ', .icon-' + item.name.replace(/roll$/i, ".hover");
+            }
+
+            if (item.name.match(/-over$/i)) {
+              item.name = item.name.replace(/-over$/i, ":hover") + ', .icon-' + item.name.replace(/-over$/i, ".hover");
+            }
+
           }
+        }));
 
-          if (item.name.match(/-hover$/i)) {
-            item.name = item.name.replace(/-hover$/i, ":hover") + ', .icon-' + item.name.replace(/-hover$/i, ".hover");
-          }
+        // Pipe image stream through image optimizer and onto disk
+        var imgStream = spriteData.img
+          // DEV: We must buffer our stream into a Buffer for `imagemin`
+          .pipe(buffer())
+          .pipe(imagemin({optimizationLevel: 5}))
+          .pipe(gulp.dest(config.build.assets + 'sprites'));
 
-          if (item.name.match(/roll$/i)) {
-            item.name = item.name.replace(/roll$/i, ":hover") + ', .icon-' + item.name.replace(/roll$/i, ".hover");
-          }
+        mergedImage.add(imgStream);
 
-          if (item.name.match(/-over$/i)) {
-            item.name = item.name.replace(/-over$/i, ":hover") + ', .icon-' + item.name.replace(/-over$/i, ".hover");
-          }
+        // Pipe CSS stream through CSS optimizer and onto disk
+        var cssStream = spriteData.css
+          //.pipe(csso())
+          //.pipe(gulp.dest(config.assets + 'styles'))
+          .pipe(gulp.dest(config.build.assets + 'sprites'))
+          .on('end', function() {
+            //console.log('sprite end css process');
+          });
 
-        }
-      }));
+        mergedCss.add(cssStream);
 
-      // Pipe image stream through image optimizer and onto disk
-      var imgStream = spriteData.img
-        // DEV: We must buffer our stream into a Buffer for `imagemin`
-        .pipe(buffer())
-        .pipe(imagemin({optimizationLevel: 5}))
-        .pipe(gulp.dest(config.build.assets + 'sprites'));
-
-      mergedImage.add(imgStream);
-
-      // Pipe CSS stream through CSS optimizer and onto disk
-      var cssStream = spriteData.css
-        //.pipe(csso())
-        //.pipe(gulp.dest(config.assets + 'styles'))
-        .pipe(gulp.dest(config.build.assets + 'sprites'))
-        .on('end', function() {
-          //console.log('sprite end css process');
-        });
-
-      mergedCss.add(cssStream);
+      } // isDirectory condition end
 
     });
 
